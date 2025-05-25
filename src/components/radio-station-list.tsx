b836b6f5e168fcd955c7709f1f459a70714cd570
Nathan Abela <50643013+nathan-abela@users.noreply.github.com>
2025-05-26 00:15:12 +0200

    refactor: pass stations as prop to radio station list

--- src/components/radio-station-list.tsx
+++ src/components/radio-station-list.tsx
@@ -1,12 +1,12 @@
-import { stations } from "@/lib/data/stations";
+import { RadioStation } from "@/lib/types/radio";
 
 import { RadioStationCard } from "./radio-station-card";
 
-export function RadioStationList() {
+export function RadioStationList({ stations }: { stations: RadioStation[] }) {
 	return (
 		<div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-			{stations.map((stations) => (
-				<RadioStationCard key={stations.id} station={stations} />
+			{stations.map((station) => (
+				<RadioStationCard key={station.id} station={station} />
 			))}
 		</div>
 	);
