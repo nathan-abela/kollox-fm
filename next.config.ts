70665c5fab15980fe94d7be202999568655e0359
Nathan Abela <50643013+nathan-abela@users.noreply.github.com>
2025-06-07 13:00:34 +0200

    chore: add base path and asset prefix to next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
+// Determines if the current environment is production
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-	/* config options here */
+	/**
+	 * Sets the base path for the application.
+	 * For deploying to a subdirectory (ex. GitHub Pages).
+	 * Uses '/kollox-fm' in production, empty otherwise.
+	 */
+	basePath: isProd ? "/kollox-fm" : "",
+	/**
+	 * Sets the asset prefix for the application.
+	 * For serving assets from a different domain.
+	 * Uses '/kollox-fm' in production, empty otherwise.
+	 */
+	assetPrefix: isProd ? "/kollox-fm" : "",
+	output: "export",
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
